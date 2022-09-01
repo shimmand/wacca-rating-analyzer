833cad5fe4282ba0aa1172ed3a75bb55e3a170de
shimmand <58391224+shimmand@users.noreply.github.com>
2022-09-01 22:48:59 +0900

    Addtional changes

--- js/script.js
+++ js/script.js
@@ -1475,9 +1475,9 @@ function activateAnalyzeMode() {
     playdata.value = ''
 
     // [DEBUG] FOR DEBUGGING USE ONLY
-    location.reload()
+    // location.reload()
 
-    // location.href = 'https://bit.ly/3tiGGDb'
+    location.href = 'https://bit.ly/3tiGGDb'
 }
 
 // Run the analyze
